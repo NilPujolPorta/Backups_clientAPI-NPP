1263eeb3e26861122bd32a9e0a4af9b71bf11f14
Nil Pujol <nilpujolporta@gmail.com>
2022-05-06 17:08:59 +0200

    Made utils.py description of methods

--- Backups_clientAPI/utils.py
+++ Backups_clientAPI/utils.py
@@ -1,16 +1,49 @@
 from datetime import datetime, timezone
 
-#Retorna el temps actual en utc timestamp
 def temps()-> int:
+	"""Gives the time in utc timestamp
+
+	Returns
+	-------
+	int
+		Current time in utc timestamp.
+
+	"""
 	dt = datetime.now(timezone.utc)
 	utc_time = dt.replace(tzinfo=timezone.utc)
 	utc_timestamp = utc_time.timestamp()
 	return(round(utc_timestamp))
 
 def utcToTime(utc_time:int)-> datetime:
+	"""Gives datetime of a given utc timestamp.
+	
+	Parameters
+	----------
+	utc_time:int
+		A certain time in utc timestamp
+
+	Returns
+	-------
+	datetime
+		datetime of the given utc timestamp.
+
+	"""
 	return datetime.utcfromtimestamp(utc_time)
 
 def statusConvertor(status:int)->str:
+	"""Gives the status of a status code from the synology api.
+	
+	Parameters
+	----------
+	status:int
+		the status code to convert
+
+	Returns
+	-------
+	String
+		The status code conveted to the status name.
+
+	"""
 	if status == 2:
 		return("Correcte")
 	elif status == 4:
@@ -20,6 +53,19 @@ def statusConvertor(status:int)->str:
 	else:
 		return("CodiDesconegut")
 def statusConvertorPandora(status:int)->str:
+	"""Gives the status of a status code from the PandoraFMS api.
+	
+	Parameters
+	----------
+	status:int
+		the status code to convert
+
+	Returns
+	-------
+	String
+		The status code conveted to the status name.
+
+	"""
 	if status == 0:
 		return("Correcte")
 	elif status == 2:
